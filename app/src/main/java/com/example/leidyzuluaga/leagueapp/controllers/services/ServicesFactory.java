98a82cf3f3e04c586ab861414e730dd8867e6fc2
Leidy-Zulu <leidyzulu9@gmail.com>
2018-09-08 13:00:14 -0500

    Retrofit implementation

--- app/src/main/java/com/example/leidyzuluaga/leagueapp/controllers/services/ServicesFactory.java
+++ app/src/main/java/com/example/leidyzuluaga/leagueapp/controllers/services/ServicesFactory.java
@@ -1,4 +1,44 @@
 package com.example.leidyzuluaga.leagueapp.controllers.services;
 
+import com.example.leidyzuluaga.leagueapp.helper.Constants;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Converter;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ServicesFactory {
+
+    private static final String API_BASE_PATH = Constants.URL_LEAGUE;
+    private final Retrofit restAdapter;
+
+
+
+    public ServicesFactory() {
+        final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(Constants.TEN, TimeUnit.SECONDS)
+                .writeTimeout(Constants.TEN, TimeUnit.SECONDS)
+                .readTimeout(Constants.TEN, TimeUnit.SECONDS).build();
+        restAdapter = new Retrofit.Builder()
+                .baseUrl(API_BASE_PATH)
+                .client(okHttpClient)
+                .addConverterFactory(getGsonConverter())
+                .build();
+    }
+
+
+    private Converter.Factory getGsonConverter() {
+        Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create();
+        return GsonConverterFactory.create(gson);
+    }
+
+
+    public Object getInstance(Class service) {
+        return restAdapter.create(service);
+    }
 }
---
+++ app/src/main/java/com/example/leidyzuluaga/leagueapp/helper/Constants.java
@@ -0,0 +1,7 @@
+package com.example.leidyzuluaga.leagueapp.helper;
+
+public class Constants {
+
+    public static final String URL_LEAGUE = "https://www.thesportsdb.com/api/v1/json/1/";
+    public static final long TEN = 10;
+}
